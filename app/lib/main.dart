5b7523af22b129c75005276c91134557170c14b4
liupan163 <18792418363@163.com>
2019-08-09 16:41:42 +0800

    upd |> set app entry file logic

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,72 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:walletassist/wallet_assist.dart';
-import 'package:app/model/Mnemonic.dart';
+import 'package:oktoast/oktoast.dart';
+import 'page/splash_page.dart';
+import 'package:fluro/fluro.dart';
+import 'routers/routers.dart';
+import 'routers/application.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() async {
-    var mneMap = await WalletAssist.mnemonicGenerate();
-    Mnemonic mnemonic = new Mnemonic();
-    mnemonic.mn = mneMap["mn"];
-    mnemonic.mnId = mneMap["mnId"];
-    mnemonic.status = mneMap["status"];
-    print("mnemonic.status is " + mnemonic.status.toString());
-    setState(() {
-      _counter++;
-    });
+  MyApp() {
+    final router = Router();
+    Routes.configureRoutes(router);
+    Application.router = router;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+    return OKToast(
+        child: MaterialApp(
+      title: "cashbox",
+      home: SplashPage(),
+    ));
   }
 }
--- app/lib/page/splash_page.dart
+++ app/lib/page/splash_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/widgets.dart';
+import '../wallet_manager/wallet_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:app/page/home_page.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -6,8 +9,28 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  var allWalletList = List();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _initWallet();
+  }
+
+  void _initWallet() async {
+    //var allWalletList = await WalletMgr.instance.loadAllWalletList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (allWalletList.length > 0) {
+      print("goHomePage=>");
+      return HomePage();
+    } else {
+      return Material(
+        child: Text("this is splashpage"),
+      );
+    }
   }
 }
