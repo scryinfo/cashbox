9575941711a3853019f3248ec34ba8f7b9d49e21
liupan163 <18792418363@163.com>
2019-08-09 14:32:56 +0800

    upd |>init flutter project structure

---
+++ app/lib/page/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
