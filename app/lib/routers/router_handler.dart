e19fb5e394017b0cf7dcbfe495ba751544165d09
liupan163 <18792418363@163.com>
2019-08-13 15:49:11 +0800

    upd |> update homepage layout

--- app/lib/routers/router_handler.dart
+++ app/lib/routers/router_handler.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 
 import '../page/splash_page.dart';
-import '../page/home_page.dart';
+import 'package:app/page/home_page/home_page.dart';
 
 Handler splashPageHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) {
