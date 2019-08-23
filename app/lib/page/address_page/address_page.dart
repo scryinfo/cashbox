40f43c6b8bdd5c683c7c243731a47053c794fca3
liupan163 <18792418363@163.com>
2019-08-23 09:40:44 +0800

    add |> add clipBoardData func

--- app/lib/page/address_page/address_page.dart
+++ app/lib/page/address_page/address_page.dart
@@ -1,4 +1,8 @@
+import 'package:app/res/resources.dart';
+import 'package:app/util/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import '../../widgets/app_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -76,11 +80,17 @@ class AddressPage extends StatelessWidget {
                           top: ScreenUtil().setHeight(6.25),
                         ),
                         alignment: Alignment.centerLeft,
-                        child: Text(
-                          content,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
+                        child: GestureDetector(
+                          onLongPress: () async {
+                            Utils.copyMsg(content);
+                            Fluttertoast.showToast(msg: "地址已经成功拷贝!~~~");
+                          },
+                          child: Text(
+                            content,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                            ),
                           ),
                         ),
                       ),
@@ -88,17 +98,16 @@ class AddressPage extends StatelessWidget {
                   ),
                 ),
               ),
+              Gaps.scaleVGap(5),
               Container(
-                margin: EdgeInsets.only(
-                  top: ScreenUtil().setHeight(18),
-                ),
                 alignment: Alignment.bottomCenter,
                 width: ScreenUtil().setWidth(41),
                 height: ScreenUtil().setHeight(9),
                 color: Color.fromRGBO(26, 141, 198, 0.20),
                 child: FlatButton(
                   onPressed: () {
-                    print("copy address ok");
+                    Utils.copyMsg(content);
+                    Fluttertoast.showToast(msg: "地址已经成功拷贝!~~~");
                   },
                   child: Text(
                     "点击复制地址",
