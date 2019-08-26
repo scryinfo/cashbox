c26b26f3fd11e54559d6992ff20285b04075bba2
liupan163 <18792418363@163.com>
2019-08-26 16:02:39 +0800

    upd |> update minepage

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,8 +11,8 @@ import 'package:app/res/resources.dart';
 void main() {
   runApp(MyApp());
   if (Platform.isAndroid) {
-    print("it is android platform===>");
-    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+    /*以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，
+    覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。*/
     SystemUiOverlayStyle systemUiOverlayStyle =
         SystemUiOverlayStyle(statusBarColor: Colors.transparent);
     SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
