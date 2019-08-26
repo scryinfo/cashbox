3d944c6f78d5000ba759009c312ce10f01fc087a
jeremy <jm140915@hotmail.com>
2019-08-26 21:42:07 +0800

    refactoring code structure

--- bc/bc-service/src/account_generate.rs
+++ bc/bc-service/src/account_generate.rs
@@ -1,7 +1,5 @@
-use substrate_primitives::{ed25519, sr25519, hexdisplay::HexDisplay, Pair, crypto::Ss58Codec};
+use substrate_primitives::{ hexdisplay::HexDisplay, Pair,crypto::Ss58Codec};
 use bip39::{Mnemonic, MnemonicType, Language};
-use substrate_bip39::mini_secret_from_entropy;
-use schnorrkel::keys::MiniSecretKey;
 use rand::{RngCore, rngs::OsRng};
 
 pub trait Crypto {
@@ -69,53 +67,7 @@ pub trait Crypto {
         }
     }
 }
-
-pub struct Ed25519;
-impl Crypto for Ed25519 {
-    type Seed = [u8; 32];
-    type Pair = ed25519::Pair;
-
-    fn seed_from_phrase(phrase: &str, password: Option<&str>) -> Self::Seed {
-        Sr25519::seed_from_phrase(phrase, password)
-    }
-    fn pair_from_suri(suri: &str, password_override: Option<&str>) -> Self::Pair {
-        ed25519::Pair::from_legacy_string(suri, password_override)
-    }
-    fn pair_from_seed(seed: &Self::Seed) -> Self::Pair { ed25519::Pair::from_seed(&seed.clone()) }
-    fn ss58_from_pair(pair: &Self::Pair) -> String { pair.public().to_ss58check() }
-    fn public_from_pair(pair: &Self::Pair) -> Vec<u8> { (&pair.public().0[..]).to_owned() }
-    fn seed_from_pair(pair: &Self::Pair) -> Option<&Self::Seed> { Some(pair.seed()) }
-}
-
-
-pub struct Sr25519;
-
-impl Crypto for Sr25519 {
-    type Seed = [u8; 32];
-    type Pair = sr25519::Pair;
-
-    fn seed_from_phrase(phrase: &str, password: Option<&str>) -> Self::Seed {
-        mini_secret_from_entropy(
-            Mnemonic::from_phrase(phrase, Language::English)
-                .unwrap_or_else(|_|
-                    panic!("Phrase is not a valid BIP-39 phrase: \n    {}", phrase)
-                )
-                .entropy(),
-            password.unwrap_or(""),
-        )
-            .expect("32 bytes can always build a key; qed")
-            .to_bytes()
-    }
-
-    fn pair_from_suri(suri: &str, password: Option<&str>) -> Self::Pair {
-        sr25519::Pair::from_string(suri, password).expect("Invalid phrase")
-    }
-
-    fn pair_from_seed(seed: &Self::Seed) -> Self::Pair {
-        MiniSecretKey::from_bytes(seed)
-            .expect("32 bytes can always build a key; qed")
-            .into()
-    }
-    fn ss58_from_pair(pair: &Self::Pair) -> String { pair.public().to_ss58check() }
-    fn public_from_pair(pair: &Self::Pair) -> Vec<u8> { (&pair.public().0[..]).to_owned() }
-}
+mod ed25519;
+mod sr25519;
+pub use sr25519::Sr25519;
+pub use ed25519::Ed25519;
---
+++ bc/bc-service/src/walletstore/db_helper.rs
@@ -0,0 +1,111 @@
+use std::{fs, path};
+use sqlite::{Connection};
+
+
+const TB_MNEMONIC: &'static str = r#"/data/data/com.example.app/files/cashbox_mnenonic.db"#;
+const TB_WALLET: &'static str = r#"/data/data/com.example.app/files/cashbox_wallet.db"#;
+
+/*const TB_MNEMONIC: &'static str = r#"./cashbox_mnenonic.db"#;
+const TB_WALLET: &'static str = r#"./cashbox_wallet.db"#;*/
+fn create_teble(table_name: &str, table_desc: &str) -> Result<(), String> {
+    // TODO 检查参数是否合理
+    //先创建对应的文件路径
+    if !path::Path::new(table_name).exists() {
+        let file_create = fs::File::create(table_name).map_err(|e| format!("{} create error:{}", table_name, e.to_string()));
+        if file_create.is_err() {
+            return Err(file_create.unwrap_err());
+        }
+    }
+
+    match Connection::open(table_name) {
+        Ok(connect) => {
+            match connect.execute(table_desc) {
+                Ok(_) => { Ok(()) }
+                Err(e) => {
+                    Err(format!("{} create error:{}", table_name, e.to_string()))
+                }
+            }
+        }
+        Err(e) => {
+            Err(format!("create databse file {} error:{}", table_name, e.to_string()))
+        }
+    }
+}
+
+pub struct DataServiceProvider {
+   pub db_hander: Connection,
+}
+
+impl Drop for DataServiceProvider {
+    fn drop(&mut self) {
+        let detach_sql = "DETACH DATABASE 'wallet'";
+        &self.db_hander.execute(detach_sql).expect("DETACH database error!");
+    }
+}
+
+impl DataServiceProvider {
+
+    pub fn instance() -> Result<Self, String> {
+        //1、检查对应的数据库文件是否存在
+        if fs::File::open(TB_MNEMONIC).is_err() || fs::File::open(TB_WALLET).is_err() {
+            //2、若是不存在则执行sql脚本文件创建数据库
+            let mnemonic_sql = super::table_desc::get_cashbox_mnenonic_sql();
+            let mn_database_hint = create_teble(TB_MNEMONIC, mnemonic_sql.as_str());
+            if mn_database_hint.is_err() {
+                return Err(mn_database_hint.unwrap_err());
+            }
+            //create wallet table
+            let wallet_sql = super::table_desc::get_cashbox_wallet_sql();
+            let crate_wallet_hint = create_teble(TB_WALLET, wallet_sql.as_str());
+            if crate_wallet_hint.is_err() {
+                return Err(crate_wallet_hint.unwrap_err());
+            }
+        }
+
+        //start connect mnemonic database
+        match Connection::open(TB_MNEMONIC) {
+            Ok(conn) => {
+                let attach_sql = format!("ATTACH DATABASE \"{}\" AS wallet;", TB_WALLET);
+                match conn.execute(&attach_sql) {
+                    Ok(_) => {
+                        let provider = DataServiceProvider {
+                            db_hander: conn,
+                        };
+                        Ok(provider)
+                    }
+                    Err(e) => {
+                        Err(format!("ATTACH DATABASE error:{}", e.to_string()))
+                    }
+                }
+            }
+            Err(e) => {
+                let hint = format!("open TB_MNEMONIC error:{}", e.to_string());
+                Err(hint)
+            }
+        }
+    }
+
+    pub fn tx_begin(&mut self) -> Result<(), String> {
+        self.db_hander.execute("begin;").map(|_| ()).map_err(|err| err.to_string())
+    }
+
+    pub fn tx_commint(&mut self) -> Result<(), String> {
+        self.db_hander.execute("commit;").map(|_| ()).map_err(|err| err.to_string())
+    }
+
+/*
+    pub fn update_mnemonic(&self, mn: TbMnemonic) -> Result<(), String> {
+        let mn_sql = "update Mnemonic set mnemonic=? where id=?;";
+        let mut statement = self.db_hander.prepare(mn_sql).expect("sql statement is error!");
+        statement.bind(1, mn.mnemonic.unwrap().as_str()).expect(" mnemonic  bind error");
+        statement.bind(2, mn.id.unwrap().as_str()).expect("mn id bind error");
+
+        match statement.next() {
+            Ok(_state) => {
+                Ok(())
+            }
+            Err(e) => Err(e.to_string())
+        }
+    }*/
+
+}
