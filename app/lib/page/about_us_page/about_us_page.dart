b9943b261bc071fc2573dbd48b10e8629b01699c
liupan163 <18792418363@163.com>
2019-08-26 10:20:18 +0800

    add |> add simple info about transaction detail page

---
+++ app/lib/page/about_us_page/about_us_page.dart
@@ -0,0 +1,26 @@
+import 'package:app/model/Digit.dart';
+import 'package:app/model/rate.dart';
+import 'package:app/routers/fluro_navigator.dart';
+import 'package:app/routers/routers.dart';
+import 'package:app/widgets/app_bar.dart';
+import 'package:app/widgets/my_separator_line.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_easyrefresh/ball_pulse_footer.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../res/resources.dart';
+
+class AboutUsPage extends StatefulWidget {
+  @override
+  _AboutUsPageState createState() => _AboutUsPageState();
+}
+
+class _AboutUsPageState extends State<AboutUsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("about us page"),
+    );
+  }
+}
