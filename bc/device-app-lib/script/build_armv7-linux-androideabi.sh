8b7ee8bbb96885a4c0597173edcc47dcc781a88f
jeremy <jm140915@hotmail.com>
2019-08-27 10:19:14 +0800

    refactoring code structure

--- bc/device-app-lib/script/build_armv7-linux-androideabi.sh
+++ bc/device-app-lib/script/build_armv7-linux-androideabi.sh
@@ -1,4 +1,5 @@
 #！/bin/bash
 export AR=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-ar
 export CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi24-clang
+cd ..
 cargo build --target armv7-linux-androideabi
--- bc/wallet/src/lib.rs
+++ bc/wallet/src/lib.rs
@@ -1,12 +1,64 @@
-// 用于实现对各个模块的封装，实现ios、android jni接口的封装
-pub mod wallet;
-pub mod chain;
-pub mod digit;
+#[macro_use]
+extern crate serde_derive;
+
+pub mod account_generate;
+pub mod walletstore;
+pub mod module;
+
+
+#[derive(PartialEq)]
+pub enum StatusCode {
+    DylibError = -1,
+    OK = 200,
+    //正常
+    FailToGenerateMnemonic = 100,
+    //生成助记词失败
+    PwdIsWrong,
+    //密码错误
+    FailToRestPwd,
+    //重置密码失败
+    GasNotEnough,
+    //GAS费不足
+    BroadcastOk,
+    //广播上链成功
+    BroadcastFailure,  //广播上链失败
+}
+
+pub enum ChainType {
+    BTC = 1,
+    BtcTest,
+    ETH,
+    EthTest,
+    EEE,
+    EeeTest,
+    OTHER,
+}
+
+impl From<i64> for ChainType {
+    fn from(chain_type: i64) -> Self {
+        match chain_type {
+            1 => ChainType::BTC,
+            2 => ChainType::BtcTest,
+            3 => ChainType::ETH,
+            4 => ChainType::EthTest,
+            5 => ChainType::EEE,
+            6 => ChainType::EeeTest,
+            _ => ChainType::OTHER,
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::crate_mnemonic;
+    use crate::account_crypto::Ed25519;
+    use crate::account_crypto::Sr25519;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn verify_mnemonic_create() {
+        println!("{}", module::wallet::crate_mnemonic::<Ed25519>(18));
+        println!("{}", module::wallet::crate_mnemonic::<Sr25519>(18));
     }
 }
