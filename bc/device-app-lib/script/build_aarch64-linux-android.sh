1c1682b7f39b521e4ce217931d20d8f3969803b2
Peacess <peace@163.com>
2019-08-27 14:51:36 +0800

    really path for script

--- bc/device-app-lib/script/build_aarch64-linux-android.sh
+++ bc/device-app-lib/script/build_aarch64-linux-android.sh
@@ -1,5 +1,8 @@
 #！/bin/bash
-export AR=$ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar
+cuPath=$(pwd)
+batPath=$(dirname $(readlink -f "$0"))
+export AR=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar
 export CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android23-clang
-cd ..
+cd $batPath/..
 cargo build --target aarch64-linux-android
+cd $cuPath
--- bc/device-app-lib/script/build_armv7-linux-androideabi.bat
+++ bc/device-app-lib/script/build_armv7-linux-androideabi.bat
@@ -1,11 +1,14 @@
 
 set HOST_TAG=armv7-linux-androideabi
 set NDK=%ANDROID_NDK%
+set cuPath=%cd
+set batPath=%~dp0
 
 set TOOLCHAIN=%NDK%/toolchains/llvm/prebuilt/windows-x86_64
 set AR=%TOOLCHAIN%/bin/arm-linux-androideabi-ar.exe
 set CC=%TOOLCHAIN%/bin/armv7a-linux-androideabi24-clang.cmd
 set CXX=%TOOLCHAIN%/bin/armv7a-linux-androideabi24-clang++.cmd
 rustup default stable-gnu
-cd ..
+cd %batPath%/..
 cargo build --target %HOST_TAG%
+cd %cuPath%
