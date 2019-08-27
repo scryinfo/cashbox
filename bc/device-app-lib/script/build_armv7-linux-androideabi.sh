a20e7c94a1486eb6e35076a24ccc7c0048256509
Peacess <peacess@163.com>
2019-08-27 14:51:36 +0800

    really path for script

--- bc/device-app-lib/script/build_armv7-linux-androideabi.sh
+++ bc/device-app-lib/script/build_armv7-linux-androideabi.sh
@@ -1,5 +1,8 @@
 #！/bin/bash
+cuPath=$(pwd)
+batPath=$(dirname $(readlink -f "$0"))
 export AR=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-ar
 export CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi24-clang
-cd ..
+cd $batPath/..
 cargo build --target armv7-linux-androideabi
+cd $cuPath
