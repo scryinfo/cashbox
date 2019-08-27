4735a18bf1d56225bb20226506250eb12a31b41e
liupan163 <18792418363@163.com>
2019-08-27 14:56:59 +0800

    upd |> update code

--- app/lib/model/digit.dart
+++ app/lib/model/digit.dart
@@ -6,15 +6,34 @@ class Digit {
   String shortName; //缩写名称
   String fullName; //全名
   String balance; //数量
-  String money; //金额
-  DigitRate digitRate; //市场价格
-  String address; //代币地址
-  String contractAddress; //合约地址
-  String urlImg; //代币图标icon地址
-  bool isVisible; //是否可见
   int decimal; //精度，小数点后的位数
+  String money; //金额 = 市场价格rate * 数量balance
+  String address; //代币地址
+  String urlImg; //代币图标icon地址
+  bool isVisible; //代币是否可见
+  DigitRate digitRate; //市场价格
   String lastTxInfo; //链上最后一笔交易
-  Digit(chainId) {
+}
+
+class EeeDigit extends Digit {
+  EeeDigit(digitId, chainId) {
     this.chainId = chainId;
+    this.digitId = digitId;
   }
+}
+
+class BtcDigit extends Digit {
+  BtcDigit(digitId, chainId) {
+    this.chainId = chainId;
+    this.digitId = digitId;
+  }
+}
+
+class EthDigit extends Digit {
+  EthDigit(digitId, chainId) {
+    this.chainId = chainId;
+    this.digitId = digitId;
+  }
+
+  String contractAddress; //合约地址
 }
--- app/lib/util/utils.dart
+++ app/lib/util/utils.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/services.dart';
 
 class Utils {
+  ///复制信息
   static copyMsg(String msg) {
     Clipboard.setData(ClipboardData(text: msg));
   }
 
+  ///粘贴信息
   static Future<ClipboardData> getCopyMsg() async {
     return Clipboard.getData(Clipboard.kTextPlain);
   }
