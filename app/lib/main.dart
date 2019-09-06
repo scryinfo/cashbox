e47f6ec4382db87792a0298e88ba746c6569f263
liupan163 <18792418363@163.com>
2019-09-06 15:09:13 +0800

    upd |> update code

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,14 +11,17 @@ import 'routers/routers.dart';
 import 'routers/application.dart';
 import 'package:app/res/resources.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
+  //debugPaintLayerBordersEnabled=true;测试样式边界用
+  //debugPaintBaselinesEnabled=true;
+
   runApp(MyApp());
   if (Platform.isAndroid) {
     /*以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，
     覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。*/
-    SystemUiOverlayStyle systemUiOverlayStyle =
-        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
     SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
   }
 }
