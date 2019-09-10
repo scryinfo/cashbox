f9b28348a5d9c77bdaacbd3d71bd1d0fa14a47a6
liupan163 <18792418363@163.com>
2019-09-10 11:28:30 +0800

    upd |> fix entry layout problem

--- app/lib/page/entry_page.dart
+++ app/lib/page/entry_page.dart
@@ -1,16 +1,13 @@
-import 'dart:typed_data';
-
-import 'package:app/model/wallet.dart';
 import 'package:app/res/resources.dart';
 import 'package:app/routers/fluro_navigator.dart';
 import 'package:app/routers/routers.dart';
+import 'package:app/util/log_util.dart';
 import 'package:flutter/widgets.dart';
 import 'package:app/model/wallets.dart';
 import 'package:flutter/material.dart';
 import 'package:app/page/eee_page/eee_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import '../test/wallet_manager_test.dart';
 
 class EntryPage extends StatefulWidget {
   @override
@@ -21,6 +18,7 @@ class _EntryPageState extends State<EntryPage> {
   var allWalletList = List();
   bool _agreeServiceProtocol = true;
   bool isContainWallet = false;
+  Future future;
 
   @override
   void initState() {
@@ -30,14 +28,16 @@ class _EntryPageState extends State<EntryPage> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    checkContainWallet();
+    _initData(); //case 删除钱包后，没钱包，回到entryPage
   }
 
-  void checkContainWallet() async {
+  void _initData() async {
+    future = _checkIsContainWallet();
+  }
+
+  Future<bool> _checkIsContainWallet() async {
     isContainWallet = await Wallets.instance.isContainWallet();
-    setState(() {
-      this.isContainWallet = isContainWallet;
-    });
+    return isContainWallet;
   }
 
   @override
@@ -45,12 +45,38 @@ class _EntryPageState extends State<EntryPage> {
     ///初始化屏幕 宽高比 ，以 cashbox切图中，标注XXXHDPI@4x 为准
     ScreenUtil.instance = ScreenUtil(width: 90, height: 160)..init(context);
 
-    //todo 2.0  区分链类型，加载对应链界面
-    if (isContainWallet) {
-      return EeePage();
-    } else {
-      return _buildProtocolLayout();
-    }
+    return Container(
+      child: FutureBuilder(
+          future: future,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print("snapshot.error==>" + snapshot.error.toString());
+              LogUtil.e("digitList future snapshot.hasError is +>", snapshot.error.toString());
+              return Center(
+                child: Text(
+                  "数据加载出错了，请尝试重新加载!~",
+                  style: TextStyle(color: Colors.white70),
+                ),
+              );
+            }
+            if (snapshot.hasData) {
+              bool isContainWallet = snapshot.data;
+              if (isContainWallet) {
+                return EeePage(); //已有钱包  || todo 2.0  区分链类型，加载对应链界面
+              } else {
+                return _buildProtocolLayout();
+              }
+            }
+            return Container(
+              width: ScreenUtil().setWidth(90),
+              height: ScreenUtil().setHeight(160),
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/images/bg_loading.png"), fit: BoxFit.fill),
+              ),
+              child: null,
+            );
+          }),
+    );
   }
 
   Widget _buildProtocolLayout() {
