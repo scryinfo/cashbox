65d6f4d4d75b1bb3419645c5c8c5213b7ee64766
liupan163 <18792418363@163.com>
2019-09-25 14:32:19 +0800

    upd |> update code

---
+++ app/lib/demo/flutter_webview_plugin_demo.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class FlutterWebViewPluginDemo extends StatefulWidget {
+  @override
+  _FlutterWebViewPluginDemo createState() => _FlutterWebViewPluginDemo();
+}
+
+class _FlutterWebViewPluginDemo extends State<FlutterWebViewPluginDemo> {
+  String localHtmlFilPath = "";
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    initListen();
+  }
+
+  void initListen() {
+    //  监听url地址改变事件
+    flutterWebviewPlugin.onUrlChanged.listen((String url) {
+      print("flutterWebviewPlugin url =========>" + url);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("html demo")),
+      body: Container(
+        width: ScreenUtil.instance.setWidth(90),
+        height: ScreenUtil.instance.setHeight(160),
+        color: Colors.blueAccent,
+        child: WebviewScaffold(
+          url: "file:///android_asset/flutter_assets/assets/dist/index.html",
+          appBar: AppBar(
+            title: const Text('Widget WebView'),
+          ),
+          withZoom: true,
+          withLocalStorage: true,
+          hidden: true,
+          initialChild: Container(
+            color: Colors.redAccent,
+            child: const Center(
+              child: Text('Waiting.....'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
