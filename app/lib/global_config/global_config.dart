50679dbe77d343d484202fee6afba82bc5d8f131
parker <18792418363@163.com>
2020-02-22 15:46:09 +0800

    upd |> upadte load tx and display

--- app/lib/global_config/global_config.dart
+++ app/lib/global_config/global_config.dart
@@ -7,7 +7,7 @@ class GlobalConfig {
   static const Erc20GasLimitKey="eth";
   static const EthGasPriceKey="eth";
   static const Erc20GasPriceKey="eth";
-  static Map<String, double> maxGasLimitMap = {EthGasLimitKey: 30000, Erc20GasLimitKey: 300000};
+  static Map<String, double> maxGasLimitMap = {EthGasLimitKey: 30000, Erc20GasLimitKey: 300000};//unit: gwei
   static Map<String, double> minGasLimitMap = {EthGasLimitKey: 21000, Erc20GasLimitKey: 35000};
   static Map<String, double> defaultGasLimitMap = {EthGasLimitKey: 21000, Erc20GasLimitKey: 70000};
   static Map<String, double> maxGasPriceMap = {EthGasPriceKey: 30, Erc20GasPriceKey: 20};
@@ -77,22 +77,24 @@ const Erc20_Balance = "http://api-cn.etherscan.com/api?module=account&action=tok
 const Erc20_TestNet_Balance = "https://api-ropsten.etherscan.io/api?module=account&action=tokenbalance&contractaddress=";
 //http://api-cn.etherscan.com/api?module=account&action=tokenbalance&contractaddress=0x9F5F3CFD7a32700C93F971637407ff17b91c7342&address=0xe04f27eb70e025b78871a2ad7eabe85e61212761&tag=latest&apikey=XGB9RHEF6XKHIB37G5S33CWFK89XQJ5EU1
 //http://api-ropsten.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xaa638fcA332190b63Be1605bAeFDE1df0b3b031e&address=0xe04f27eb70e025b78871a2ad7eabe85e61212761&tag=latest&apikey=XGB9RHEF6XKHIB37G5S33CWFK89XQJ5EU1
-String assembleErc20BalanceUrl(String address,String contractAddress,{String netType=Erc20_Balance}) {
+String assembleErc20BalanceUrl(String address,{String contractAddress,String netType=Erc20_Balance}) {
   return netType + contractAddress + "&address=" + address + "&tag=latest&apikey=" + ETHERSCAN_API_KEY;
 }
 
 const Eth_Tx_List = "http://api-cn.etherscan.com/api?module=account&action=txlist&address=";
 const Eth_TestNet_Tx_List = "https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=";
 //http://api-cn.etherscan.com/api?module=account&action=txlist&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=XGB9RHEF6XKHIB37G5S33CWFK89XQJ5EU1
-String assembleEthTxListUrl(String address,String contractAddress,{String netType=Eth_Tx_List,String startBlock = "0",String endBlock="999999",String page="1",String offset="20"}) {
-  return netType + address + "&startblock="+startBlock+"&endblock="+endBlock+"&page="+page+"&offset="+offset+"&sort=asc&apikey=" + ETHERSCAN_API_KEY;
+//http://api-cn.etherscan.com/api?module=account&action=txlist&address=0xa4512ca7618d8d12a30C28979153aB09809ED7fD&startblock=0&endblock=999999&page=1&offset=20&sort=asc&apikey=XGB9RHEF6XKHIB37G5S33CWFK89XQJ5EU1
+String assembleEthTxListUrl(String address,{String contractAddress,String netType=Eth_Tx_List,String startBlock = "0",String endBlock="99999999",String page="1",String offset="20"}) {
+  print("url==>"+netType + address + "&startblock="+startBlock+"&endblock="+endBlock+"&page="+page+"&offset="+offset+"&sort=desc&apikey=" + ETHERSCAN_API_KEY);
+  return netType + address + "&startblock="+startBlock+"&endblock="+endBlock+"&page="+page+"&offset="+offset+"&sort=desc&apikey=" + ETHERSCAN_API_KEY;
 }
 
 const Erc20_Tx_List = "http://api-cn.etherscan.com/api?module=account&action=tokentx&contractaddress=";
 const Erc20_TestNet_Tx_List = "https://api-ropsten.etherscan.io/api?module=account&action=tokentx&contractaddress=";
 //http://api-cn.etherscan.com/api?module=account&action=tokentx&contractaddress=0x9F5F3CFD7a32700C93F971637407ff17b91c7342&address=0xa4512ca7618d8d12a30C28979153aB09809ED7fD&page=1&offset=100&sort=asc&apikey=XGB9RHEF6XKHIB37G5S33CWFK89XQJ5EU1
-String assembleErc20TxListUrl(String address,String contractAddress,{String netType=Erc20_Tx_List,String page="1",String offset="20"}) {
-  return netType + contractAddress + "&address=" + address + "&page="+page+"&offset="+offset+"&sort=asc&apikey=" + ETHERSCAN_API_KEY;
+String assembleErc20TxListUrl(String address,{String contractAddress,String netType=Erc20_Tx_List,String page="1",String offset="20"}) {
+  return netType + contractAddress + "&address=" + address + "&page="+page+"&offset="+offset+"&sort=desc&apikey=" + ETHERSCAN_API_KEY;
 }
 
 const Eth_Tx_Account = "http://api-cn.etherscan.com/api?module=proxy&action=eth_getTransactionCount&address=";
