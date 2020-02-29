7558536f796487d4b8eef416778389fc1aef78de
liupan163 <18792418363@163.com>
2020-02-29 16:29:16 +0800

    upd |> fix load testnet erc20 txhistory display

--- app/lib/net/etherscan_util.dart
+++ app/lib/net/etherscan_util.dart
@@ -1,7 +1,7 @@
 import 'package:app/global_config/global_config.dart';
 import 'package:app/model/chain.dart';
 import 'package:app/model/tx_model/eth_transaction_model.dart';
-
+import 'dart:convert';
 import 'net_util.dart';
 
 //nonce === txAccount
@@ -96,13 +96,40 @@ Future<List<EthTransactionModel>> loadEthTxHistory(String address, ChainType cha
   }
 }
 
-//todo
-Future<List> loadErc20TxHistory(String address, String contractAddress, ChainType chainType, {String offset}) async {
-  var modelArray = [];
+Future<List<EthTransactionModel>> loadErc20TxHistory(String address, String contractAddress, ChainType chainType, {String offset}) async {
+  List<EthTransactionModel> modelArray = [];
   try {
-    var res = await request(assembleErc20TxListUrl(address, chainType, contractAddress: contractAddress, offset: offset));
-    print("loadErc20TxHistory=====================>" + res.toString());
-    //todo 解析成 erc20的格式
+    var res = await request(assembleErc20TxListUrl(address, contractAddress: contractAddress, chainType: chainType, offset: offset));
+    if (res != null && (res as Map).containsKey("result")) {
+      for (var i = 0; i < res["result"].length; i++) {
+        var ethTxModel = new EthTransactionModel();
+        ethTxModel
+          ..blockNumber = res["result"][i]["blockNumber"]
+          ..hash = res["result"][i]["hash"]
+          ..nonce = res["result"][i]["nonce"]
+          ..blockHash = res["result"][i]["blockHash"]
+          ..transactionIndex = res["result"][i]["transactionIndex"]
+          ..from = res["result"][i]["from"]
+          ..to = res["result"][i]["to"]
+          ..gas = res["result"][i]["gas"]
+          ..gasPrice = res["result"][i]["gasPrice"]
+          ..isError = res["result"][i]["isError"]
+          ..txreceipt_status = res["result"][i]["txreceipt_status"]
+          ..input = res["result"][i]["input"]
+          ..contractAddress = res["result"][i]["contractAddress"]
+          ..cumulativeGasUsed = res["result"][i]["cumulativeGasUsed"]
+          ..gasUsed = res["result"][i]["gasUsed"]
+          ..confirmations = res["result"][i]["confirmations"];
+        ethTxModel.timeStamp = DateTime.fromMillisecondsSinceEpoch(int.parse(res["result"][i]["timeStamp"]) * 1000).toString();
+        if (res["result"][i]["from"].trim().toLowerCase() == address.trim().toLowerCase()) {
+          ethTxModel.value = "-" + (double.parse(res["result"][i]["value"]) / Eth_Unit).toString();
+        } else {
+          ethTxModel.value = "+" + (double.parse(res["result"][i]["value"]) / Eth_Unit).toString();
+        }
+        ethTxModel.isError = "交易成功"; //erc拿到的都会是交易成功的记录
+        modelArray.add(ethTxModel);
+      }
+    }
     return modelArray;
   } catch (e) {
     print("error is ====>" + e);
