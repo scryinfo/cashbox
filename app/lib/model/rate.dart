4ea88849c7fe2585f5fa4476998c34b6005fbebe
liupan163 <18792418363@163.com>
2020-03-05 15:09:07 +0800

    upd |> update display price and rate change

--- app/lib/model/rate.dart
+++ app/lib/model/rate.dart
@@ -1,27 +1,42 @@
 class Rate {
-  DigitRate getDigitRate() {
-    var digitRate = DigitRate();
-    return digitRate;
+  static Rate get instance => _getInstance();
+  static Rate _instance; //private
+
+  static _getInstance() {
+    if (_instance == null) {
+      _instance = new Rate._internal();
+      return _instance;
+    }
+    return _instance;
+  }
+
+  Rate._internal() {
+    // 可初始化参数
+  }
+
+  Map digitRateMap = Map<String, DigitRate>();
+
+  setDigitRateMap(Map digitRateMap) {
+    this.digitRateMap = digitRateMap;
   }
 }
 
 class DigitRate {
-  String name;
-  String symbol;
-  double price;
-  double high;
-  double low;
-  double histHigh;
-  double histLow;
-  double timestamps;
-  double volume;
-  double changeHourly;
+  String name = "";
+  String symbol = "";
+  double price = 0.0;
+  double high = 0.0;
+  double low = 0.0;
+  double histHigh = 0.0;
+  double histLow = 0.0;
+  int timestamps = 0;
+  int volume = 0;
+  double changeHourly = 0.00;
 
-  String getShortName() {
-    return symbol;
-  }
-
-  String getFullName() {
-    return name;
+  String get getChangeHour {
+    if (changeHourly > 0) {
+      return (changeHourly * 100.0).toString() + "%" + "↑";
+    }
+    return (changeHourly * 100.0).toString() + "%" + "↓";
   }
 }
