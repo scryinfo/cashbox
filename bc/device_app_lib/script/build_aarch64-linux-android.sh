79078ee5a47b4d6424bdada5183f9022ac9105f7
jeremy <jm140915@hotmail.com>
2020-03-19 09:17:04 +0800

    modify save wallet func

--- bc/device_app_lib/script/build_aarch64-linux-android.sh
+++ bc/device_app_lib/script/build_aarch64-linux-android.sh
@@ -4,5 +4,5 @@ batPath=$(dirname $(readlink -f "$0"))
 export AR=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar
 export CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android23-clang
 cd $batPath/..
-cargo build --target aarch64-linux-android
+cargo build --release --target  aarch64-linux-android
 cd $cuPath
--- bc/device_app_lib/script/build_armv7-linux-androideabi.sh
+++ bc/device_app_lib/script/build_armv7-linux-androideabi.sh
@@ -4,5 +4,5 @@ batPath=$(dirname $(readlink -f "$0"))
 export AR=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-ar
 export CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi24-clang
 cd $batPath/..
-cargo build --target armv7-linux-androideabi
+cargo build --release --target armv7-linux-androideabi
 cd $cuPath
