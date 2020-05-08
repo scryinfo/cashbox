6de71488008b678946e863b6f18e985946d7cf0e
liupan163 <18792418363@163.com>
2020-05-08 17:09:30 +0800

    upd |> update layout code

--- app/lib/page/digits_manage_page/digits_manage_page.dart
+++ app/lib/page/digits_manage_page/digits_manage_page.dart
@@ -1,4 +1,23 @@
+import 'package:app/generated/i18n.dart';
+import 'package:app/model/chain.dart';
+import 'package:app/model/digit.dart';
+import 'package:app/model/rate.dart';
+import 'package:app/model/wallet.dart';
+import 'package:app/model/wallets.dart';
+import 'package:app/provide/transaction_provide.dart';
+import 'package:app/res/styles.dart';
+import 'package:app/routers/fluro_navigator.dart';
+import 'package:app/routers/routers.dart';
+import 'package:app/util/log_util.dart';
+import 'package:app/widgets/app_bar.dart';
+import 'package:app/widgets/my_separator_line.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/ball_pulse_footer.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:provider/provider.dart';
 
 class DigitsManagePage extends StatefulWidget {
   @override
@@ -6,10 +25,209 @@ class DigitsManagePage extends StatefulWidget {
 }
 
 class _DigitsManagePageState extends State<DigitsManagePage> {
+  static int singleDigitCount = 20; //单页面显示20条数据，一次下拉刷新更新20条
+  Wallet nowWalletM;
+  Chain nowChain;
+  String nowChainAddress = "";
+  List<Digit> nowChainDigitsList = [];
+  List<Digit> displayDigitsList = [];
+  Widget checkedWidget = Image.asset("assets/images/ic_checked.png");
+  Widget addWidget = Image.asset("assets/images/ic_plus.png");
+
+  @override
+  void initState() {
+    super.initState();
+    initData();
+  }
+
+  initData() async {
+    nowWalletM = Wallets.instance.nowWallet;
+    if (nowWalletM != null) {
+      nowChain = nowWalletM.getNowChainM();
+    }
+    if (nowChain != null) {
+      //todo  根据链类型，去访问后台代币列表,获得 nowChainDigitsList
+      nowChainDigitsList = nowChain.digitsList;
+    }
+    await loadDisplayDigitListData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("digitmanagerPage"),
+      width: ScreenUtil().setWidth(90),
+      height: ScreenUtil().setHeight(120),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: MyAppBar(
+          centerTitle: S.of(context).digit_list_title,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Container(
+            child: Column(
+          children: <Widget>[Gaps.scaleVGap(5), _digitListAreaWidgets()],
+        )),
+      ),
     );
+  }
+
+  Widget _digitListAreaWidgets() {
+    return Container(
+      height: ScreenUtil().setHeight(78),
+      width: ScreenUtil().setWidth(90),
+      child: Container(
+        padding: EdgeInsets.only(left: ScreenUtil().setWidth(3), right: ScreenUtil().setWidth(3)),
+        child: _digitListWidgets(),
+      ),
+    );
+  }
+
+  //代币列表layout
+  Widget _digitListWidgets() {
+    return EasyRefresh.custom(
+      footer: BallPulseFooter(),
+      slivers: <Widget>[
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Container(
+                child: _makeDigitListItem(index),
+              );
+            },
+            childCount: displayDigitsList.length,
+          ),
+        ),
+      ],
+      onLoad: () async {
+        //代币列表栏，下拉 刷新||加载 数据。
+        await Future.delayed(
+          Duration(seconds: 2),
+          () {
+            setState(() {
+              if (displayDigitsList.length < nowChainDigitsList.length) {
+                // 从JNI加载的数据(nowChain.digitList),还有没显示完的，继续将nowChainDigitsList剩余数据，
+                // 添加到 displayDigitsList里面做展示
+                loadDisplayDigitListData(); //下拉刷新的时候，加载新digit到displayDigitsList
+              } else {
+                // todo 2.0。 功能：加载代币列表，可选择添加erc20代币。 代币列表下拉刷新。
+                // 继续调jni获取，或者提示已经没数据了。 根据是否jni分页处理来决定。
+                Fluttertoast.showToast(msg: S.of(context).load_finish_wallet_digit.toString());
+                return;
+              }
+            });
+          },
+        );
+      },
+    );
+  }
+
+  //每个代币的layout
+  Widget _makeDigitListItem(index) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          height: ScreenUtil().setHeight(17),
+          margin: EdgeInsets.only(
+            left: ScreenUtil().setWidth(3),
+            right: ScreenUtil().setWidth(3),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              //todo 改变保存代币状态      digit.isVisible
+              print("click to 改变保存代币状态");
+              setState(() {
+                displayDigitsList[index].isVisible = !displayDigitsList[index].isVisible;
+              });
+            },
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: ScreenUtil().setWidth(10),
+                  height: ScreenUtil().setWidth(10),
+                  child: displayDigitsList[index].isVisible ? addWidget : checkedWidget,
+                ),
+                Container(
+                  width: ScreenUtil().setWidth(10),
+                  height: ScreenUtil().setWidth(10),
+                  child: Image.asset("assets/images/ic_eth.png"),
+                ),
+                Container(
+                  color: Colors.transparent,
+                  padding: EdgeInsets.only(
+                    top: ScreenUtil().setHeight(3),
+                    left: ScreenUtil().setHeight(3),
+                  ),
+                  width: ScreenUtil().setWidth(30),
+                  height: ScreenUtil().setHeight(10),
+                  child: Text(
+                    displayDigitsList[index].shortName ?? "",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: ScreenUtil.instance.setSp(3.5),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          width: ScreenUtil().setWidth(75),
+          height: ScreenUtil().setHeight(0.1),
+          child: CustomPaint(
+            foregroundPainter: MySeparatorLine(
+              lineColor: Colors.blueAccent,
+              width: ScreenUtil().setWidth(75),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Future<List<Digit>> loadDisplayDigitListData() async {
+    if (displayDigitsList.length == 0) {
+      //没有展示数据
+      if (nowChainDigitsList.length < singleDigitCount) {
+        //加载到的不够一页，全展示
+        addDigitToDisplayList(nowChainDigitsList.length);
+      } else {
+        //超一页，展示singleDigitCount个。
+        addDigitToDisplayList(singleDigitCount);
+      }
+    } else {
+      //有展示数据，继续往里添加
+      if (nowChainDigitsList.length - displayDigitsList.length > singleDigitCount) {
+        //剩余的超过一页
+        addDigitToDisplayList(singleDigitCount);
+      } else {
+        //剩余的不够一页，全给加入进去。
+        addDigitToDisplayList(nowChainDigitsList.length - displayDigitsList.length);
+      }
+    }
+    return displayDigitsList;
+  }
+
+  List<Digit> addDigitToDisplayList(int targetCount) {
+    for (var i = displayDigitsList.length; i < targetCount; i++) {
+      var digitRate = DigitRate();
+      Digit digit = EthDigit();
+      print("addDigitToDisplayList nowChainDigitsList[i].balance===>" + nowChainDigitsList[i].balance.toString());
+      digit
+        ..chainId = nowChainDigitsList[i].chainId
+        ..decimal = nowChainDigitsList[i].decimal
+        ..shortName = nowChainDigitsList[i].shortName
+        ..fullName = nowChainDigitsList[i].fullName
+        ..balance = nowChainDigitsList[i].balance
+        ..contractAddress = nowChainDigitsList[i].contractAddress
+        ..address = nowChainDigitsList[i].address
+        ..isVisible = nowChainDigitsList[i].isVisible
+        ..digitRate = digitRate;
+      displayDigitsList.add(digit);
+    }
+    return displayDigitsList;
   }
 }
