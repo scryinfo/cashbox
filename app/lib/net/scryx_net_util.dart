7d2fa7d172e828cd22dbce0d9376e5da9f8ff8c9
liupan163 <18792418363@163.com>
2020-05-20 14:22:59 +0800

    add |> add test load local json

--- app/lib/net/scryx_net_util.dart
+++ app/lib/net/scryx_net_util.dart
@@ -1,16 +1,35 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'net_util.dart';
+import 'package:flutter/services.dart';
 
-class ScryXNetUtil{
-    doSomething()async{
-      var paramObj ={"method":"state_getStorage","params":["0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9de1e86a9a8c739864cf3cc5ec2bea59fd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"],"id":1,"jsonrpc":"2.0"};
-      var resultinfo = await request("http://40.73.75.224:9933",formData: paramObj);
+class ScryXNetUtil {
+  Future<String> loadScryXNetUrl(context) async {
+    String loadString = await DefaultAssetBundle.of(context).loadString("assets/config/local_config.json");
+    print("ScryXNetUtil loadString===>" + loadString);
+    var jsonObject = json.decode(loadString);
+    print("ScryXNetUtil jsonObject===>" + jsonObject.toString());
+    return jsonObject["rateDigitIp"].toString();
+  }
+
+  doSomething(context) async {
+    var paramObj = {
+      "method": "state_getStorage",
+      "params": [
+        "0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9de1e86a9a8c739864cf3cc5ec2bea59fd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"
+      ],
+      "id": 1,
+      "jsonrpc": "2.0"
+    };
+    String netUrl = await loadScryXNetUrl(context);
+    if (netUrl == null || netUrl.isEmpty) {
+      return;
     }
+    var resultInfo = await request(netUrl, formData: paramObj);
+    return resultInfo;
+  }
 
-    loadRuntimeVersion(){
+  loadRuntimeVersion() {}
 
-    }
-
-    loadGenesisHash(){
-
-    }
+  loadGenesisHash() {}
 }
