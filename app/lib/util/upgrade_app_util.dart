83341ba21d139a54e9f20f8aae0d4cb4dcba519d
liupan163 <18792418363@163.com>
2020-05-21 14:39:02 +0800

    add |> add upgrade file

---
+++ app/lib/util/upgrade_app_util.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/services.dart';
+
+class UpgradeAppUtil {
+  static const methodPlugin = const MethodChannel('upgrade_app_channel');
+
+  //工厂单例类实现
+  factory UpgradeAppUtil() => _getInstance();
+
+  static UpgradeAppUtil get instance => _getInstance();
+  static UpgradeAppUtil _instance;
+
+  UpgradeAppUtil._internal() {
+    //init data
+  }
+
+  static UpgradeAppUtil _getInstance() {
+    if (_instance == null) {
+      _instance = new UpgradeAppUtil._internal();
+    }
+    return _instance;
+  }
+
+  static doUpgradeApp(String downloadUrl) {
+    methodPlugin.invokeMethod('upgradeApp', {'downloadurl': downloadUrl});
+  }
+}
