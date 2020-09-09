658dbd715a353f2c981a92a0abdb27a33b41551a
liupan163 <18792418363@163.com>
2020-09-09 09:38:11 +0800

    upd |> visible eee chain info

--- app/lib/model/wallets.dart
+++ app/lib/model/wallets.dart
@@ -164,7 +164,7 @@ class Wallets {
           ..chainId = eeeChain["chainId"]
           ..chainAddress = eeeChain["chainAddress"]
           ..chainType = Chain.intToChainType(eeeChain["chainType"])
-          ..isVisible = false //todo change Visible state
+          ..isVisible = true
           ..walletId = eeeChain["walletId"];
         List eeeChainDigitList = eeeChain["eeeChainDigitList"];
         for (int j = 0; j < eeeChainDigitList.length; j++) {
